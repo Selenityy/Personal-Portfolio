d4784dc910c6440918863608bfb1d1cbc4a7b29c
Selena <selena.k.rodriguez@gmail.com>
2023-07-29 18:32:09 -0500

    created the home section

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import { addHElement, createImg, createNewDiv } from "../components/DOMlogic";
+import profilePic from "../assets/star-guardian.png";
+
+const homeSection = () => {
+  let parentDiv = document.getElementById("mainContentHome");
+  let profilePicImg = document.createElement("img");
+  profilePicImg.setAttribute("id", "profilePic");
+  profilePicImg.src = profilePic;
+  parentDiv.appendChild(profilePicImg);
+
+  addHElement("h2", "mainContentHome", "homeName", "Selena Rodriguez");
+  addHElement("h5", "mainContentHome", "homeJob", "Front End Developer");
+};
+
+export { homeSection };
