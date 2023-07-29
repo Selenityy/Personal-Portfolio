d704b8b99842dcf1d74cab0b1d8cd45c5b7d5166
Selena <selena.k.rodriguez@gmail.com>
2023-07-29 18:31:53 -0500

    calls the nav and main section function

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import { createMobileNavBar } from "./components/mobileNavBar";
+import { createMobileMainContent } from "./components/mobileMainContent";
+
+createMobileNavBar();
+createMobileMainContent();
