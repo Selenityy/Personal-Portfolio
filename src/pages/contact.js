9f9d694b2cedb0110fd1171ff35fc5b0dfa77aff
Selena <selena.k.rodriguez@gmail.com>
2023-07-30 14:56:36 -0500

    transfered logos to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,6 @@
-import { addHElement } from "../components/DOMlogic";
+import { addHElement, createNewDiv } from "../components/DOMlogic";
+import gitHubLogo from "../assets/Logos/github-mark.png";
+import linkedInLogo from "../assets/Logos/linkedin-logo.png";
 
 const contactSection = () => {
   let parentDiv = document.getElementById("mainContentContact");
@@ -13,6 +15,23 @@ const contactSection = () => {
     "contactDescription",
     "If you have any opportunities or questions, please leave your details below."
   );
+
+  createNewDiv("contactForm", "mainContentContact");
+
+  createNewDiv("contactLogoIcons", "mainContentContact");
+  let logos = document.getElementById("contactLogoIcons");
+
+  let gitHub = document.createElement("img");
+  gitHub.setAttribute("class", "contactLogos");
+  gitHub.setAttribute("id", "gitHub");
+  gitHub.src = gitHubLogo;
+  logos.appendChild(gitHub);
+
+  let linkedIn = document.createElement("img");
+  linkedIn.setAttribute("class", "contactLogos");
+  linkedIn.setAttribute("id", "linkedIn");
+  linkedIn.src = linkedInLogo;
+  logos.appendChild(linkedIn);
 };
 
 export { contactSection };
